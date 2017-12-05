4ed2abd3ec7db166f12b23b29f30f6f48e922f17
Sebastian Porto <s@porto5.com>
2017-12-05 21:52:00 +1100

    Part 2 nim

--- 02-nim/main.nim
+++ 02-nim/main.nim
@@ -1,14 +1,21 @@
-import os, sequtils, strutils
+import os, sequtils, strutils, math
 
-proc processLine(line: string): int =
+proc processLine(line: string): float =
   let nums = line
     .splitWhitespace()
-    .map(parseInt)
+    .map(parseFloat)
 
-  let min = foldl(nums, min(a, b))
-  let max = foldl(nums, max(a, b))
+  # part 1
+  # let min = foldl(nums, min(a, b))
+  # let max = foldl(nums, max(a, b))
 
-  max - min
+  # max - min
+
+  # part 2
+  for i in nums:
+    for j in nums:
+      if fmod(j, i) == 0.0 and j != i:
+        result = j / i
 
 let input = readFile("input.txt")
 
