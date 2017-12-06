0cae9bf29c3e99e314d40b7e25e86c74446e0a1f
Sebastian Porto <s@porto5.com>
2017-12-06 21:02:09 +1100

    Update main.kt

--- 06-kotlin/main.kt
+++ 06-kotlin/main.kt
@@ -41,8 +41,6 @@ fun distribute(count: Int, seen: HashMap<String, Int>, list: ArrayList<Int>): In
     val key = newList.joinToString("-")
     val seenIn = seen.get(key)
 
-    println("seenIn $seenIn")
-
     if (seenIn == null) {
         seen.put(key, count)
         return distribute(count + 1, seen, newList)
@@ -53,7 +51,6 @@ fun distribute(count: Int, seen: HashMap<String, Int>, list: ArrayList<Int>): In
 
 fun main(args: Array<String>) {
     val seen = hashMapOf("" to 1)
-    // val input = arrayListOf(0, 2, 7, 0)
     val input = arrayListOf(2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14)
 
     val res = distribute(0, seen, input)
