a7846c69264eb28e9303a84837d37ca92b4890b7
Sebastian Porto <s@porto5.com>
2017-12-06 20:30:15 +1100

    Add Day 6 in Kotlin

---
+++ 06/main.kt
@@ -0,0 +1,60 @@
+fun getNextBank(list: ArrayList<Int>): Int {
+    var maxIndex = 0
+    var maxVal = 0 
+
+   for ((index, value) in list.withIndex()) {
+        if (value > maxVal) {
+            maxIndex = index
+            maxVal = value
+        }
+    }
+
+    return maxIndex
+}
+
+fun distributeNextBlock(blocksToDistribute: Int, prevBlockUsed: Int, list: ArrayList<Int>): ArrayList<Int> {
+    return when(blocksToDistribute) {
+        0 ->
+            list
+        else -> {
+            val position = if (prevBlockUsed == list.size - 1) {
+                0
+            } else {
+                prevBlockUsed + 1
+            }
+
+            val newVal = list.get(position) + 1
+            list.set(position, newVal) 
+            distributeNextBlock(blocksToDistribute - 1, position, list)
+        }
+    }
+}
+
+fun distribute(count: Int, seen: HashMap<String, Boolean>, list: ArrayList<Int>): Int {
+    val nextIndex = getNextBank(list)
+    val blocksToDistribute = list[nextIndex]
+
+    list.set(nextIndex, 0)
+
+    val newList = distributeNextBlock(blocksToDistribute, nextIndex, list)
+
+    val key = newList.joinToString("-")
+    val alreadySeen = seen.containsKey(key)
+
+    if (alreadySeen) {
+        return count + 1
+    } else {
+        seen.put(key, true)
+        return distribute(count + 1, seen, newList)
+    }
+}
+
+fun main(args: Array<String>) {
+    val seen = hashMapOf("" to true)
+    // val input = arrayListOf(0, 2, 7, 0)
+    val input = arrayListOf(2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14)
+
+    val res = distribute(0, seen, input)
+
+    println(res)
+}
