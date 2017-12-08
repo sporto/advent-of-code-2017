7bc4c342e83a57c4a32b48fc301614732a8d1695
Sebastian Porto <s@porto5.com>
2017-12-08 11:44:22 +1100

    Update main.ml

--- 07/main.ml
+++ 07/main.ml
@@ -1,1 +1,17 @@
-print_string "Hello world!\n";;
+let read_file filename = 
+    let lines =
+        ref [] in
+    let chan =
+        open_in filename in
+    try
+      while true; do
+        lines := input_line chan :: !lines
+      done; !lines
+    with End_of_file ->
+      close_in chan;
+      List.rev !lines ;;
+
+
+let lines = read_file "input.txt";;
+
+print_int (List.length lines);;
