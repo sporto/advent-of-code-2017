2cb7df66b950e65a24486faaeb8af4cfe60d42c9
Sebastian Porto <s@porto5.com>
2017-12-09 17:09:40 +1100

    Update main.swift

--- 09/main.swift
+++ 09/main.swift
@@ -1,11 +1,29 @@
 import Foundation
 
-// Read input
-let fileManager = FileManager.default
-let dir = fileManager.currentDirectoryPath
-let fileName = "input.txt"
-let filePath = dir.appending("/" + fileName)
-let fileURL = URL(fileURLWithPath: filePath)
-let content = try String(contentsOf: fileURL, encoding: .utf8)
+func getInput() -> String {
+	let fileManager = FileManager.default
+	let dir = fileManager.currentDirectoryPath
+	let fileName = "input.txt"
+	let filePath = dir.appending("/" + fileName)
+	let fileURL = URL(fileURLWithPath: filePath)
+	var content = ""
 
-print(content)
+	do {
+		content = try String(contentsOf: fileURL, encoding: .utf8)
+	}
+	catch let error as NSError {
+		print("An error took place: \(error)")
+	}
+
+	return content
+}
+
+func cleanGarbage(input: String) -> String {
+	return input
+}
+
+
+let input = getInput()
+let clean = cleanGarbage(input: input)
+
+print(clean)
